91bfbcea34d7355a6757516a5ceb2756a542d2f7
celee <show3070@gmail.com>
2024-06-14 07:44:17 +0900

    #49. GenericForeignKey 활용 - 1
    범용 외래키 기본

---
+++ weblog/models.py
@@ -0,0 +1,21 @@
+from django.contrib.contenttypes.fields import GenericForeignKey, GenericRelation
+from django.contrib.contenttypes.models import ContentType
+from django.db import models
+
+
+# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    comment_set = GenericRelation("Comment", related_query_name="post")
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    comment_set = GenericRelation("Comment", related_query_name="article")
+
+
+class Comment(models.Model):
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey(ct_field="content_type", fk_field="object_id")
+    message = models.TextField()
