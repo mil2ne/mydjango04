22a293a1ab1eb139c2084719c32941b98cf89358
celee <show3070@gmail.com>
2024-06-14 08:48:41 +0900

    #53. django 생명주기
    post_delete

--- mysite/settings.py
+++ mysite/settings.py
@@ -154,6 +154,10 @@ USE_TZ = False
 
 STATIC_URL = "static/"
 
+# Media
+# MEDIA_URL = "media/
+# MEDIA_ROOT = BASE_DIR / "mediafiles"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
