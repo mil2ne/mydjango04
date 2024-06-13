73d26c3ec456301050e3dc2e0ebf81f93dbf440a
celee <show3070@gmail.com>
2024-06-14 08:19:21 +0900

    #51. GenericForeignKey 활용 -3
    on_delete 삭제정책 흉내
    pre_delete

--- weblog/models.py
+++ weblog/models.py
@@ -2,12 +2,19 @@ from django.contrib.contenttypes.fields import GenericForeignKey, GenericRelatio
 from django.contrib.contenttypes.models import ContentType
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
+from django.db.models.signals import pre_delete
+from django.dispatch import receiver
 
 
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=100)
     comment_set = GenericRelation("Comment", related_query_name="post")
+
+
+@receiver(pre_delete, sender=Post)
+def set_value_or_delete(sender, instance: Post, **kwargs):
+    instance.comment_set.update(object_id=5)
 
 
 class Article(models.Model):
