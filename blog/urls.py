9622ff8b84a63cff244b85de041bff047a4e63cb
celee <show3070@gmail.com>
2024-06-14 11:51:41 +0900

    #56. 장고 form -2
    파일 업로드 요청

--- blog/urls.py
+++ blog/urls.py
@@ -6,6 +6,7 @@ app_name = "blog"
 urlpatterns = [
     path("", views.post_list, name="post_list"),
     path("search/", views.search, name="search"),
+    path("new/", views.post_new, name="post_new"),
     path("premium-user-guide/", views.premium_user_guide, name="premium_user_guide"),
     path("premium/<str:slug>/", views.post_premium_detail, name="post_premium_detail"),
     path("<str:slug>/", views.post_detail, name="post_detail"),
