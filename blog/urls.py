8d40bea58c7cca363a9ec42d46cd45ec57ba3d63
celee <show3070@gmail.com>
2024-06-14 11:33:02 +0900

    #55. 장고 form - 1
    html form 및 css 기본
    form 전송

--- blog/urls.py
+++ blog/urls.py
@@ -4,8 +4,8 @@ from . import views
 app_name = "blog"
 
 urlpatterns = [
-    # path("<int:pk>/", views.post_detail, name="post_detail"),
     path("", views.post_list, name="post_list"),
+    path("search/", views.search, name="search"),
     path("premium-user-guide/", views.premium_user_guide, name="premium_user_guide"),
     path("premium/<str:slug>/", views.post_premium_detail, name="post_premium_detail"),
     path("<str:slug>/", views.post_detail, name="post_detail"),
