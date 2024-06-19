71c92e72d66fc5b08e9025609010ebd41d0e0e02
celee <show3070@gmail.com>
2024-06-19 21:30:09 +0900

    #64.장고 기본 cbv api
    CreateView, UpdateView 를 활용한 생성-수정 요청 처리

--- weblog/views.py
+++ weblog/views.py
@@ -1,7 +1,7 @@
 from django.core.files.uploadedfile import UploadedFile
 from django.shortcuts import render, redirect, get_object_or_404
 
-from vanilla import FormView
+from vanilla import FormView, CreateView, UpdateView
 
 # from django.views.generic import FormView
 
@@ -9,25 +9,27 @@ from weblog.forms import PostForm
 from weblog.models import Post
 
 
-class PostCreateView(FormView):
+class PostCreateView(CreateView):
+    model = Post
     form_class = PostForm
-    template_name = "weblog/post_form.html"
+    # template_name = "weblog/post_form.html"
     success_url = "/"
 
     def form_valid(self, form):
         post = form.save(commit=False)
         post.ip = self.request.META["REMOTE_ADDR"]
-        post.save()
-        form.save_m2m()
+        # post.save()
+        # form.save_m2m()
         return super().form_valid(form)
 
 
 post_new = PostCreateView.as_view()
 
 
-class PostUpdateView(FormView):
+class PostUpdateView(UpdateView):
+    model = Post
     form_class = PostForm
-    template_name = "weblog/post_form.html"
+    # template_name = "weblog/post_form.html"
     success_url = "/"
 
     # def get_form_kwargs(self):
@@ -38,15 +40,15 @@ class PostUpdateView(FormView):
     #     form_kwargs["instance"] = instance
     #     return form_kwargs
 
-    def get_form(self, data=None, files=None, **kwargs):
-        post_pk = self.kwargs["pk"]
-        instance = get_object_or_404(Post, pk=post_pk)
-        kwargs["instance"] = instance
-        return super().get_form(data=data, files=files, **kwargs)
+    # def get_form(self, data=None, files=None, **kwargs):
+    #     post_pk = self.kwargs["pk"]
+    #     instance = get_object_or_404(Post, pk=post_pk)
+    #     kwargs["instance"] = instance
+    #     return super().get_form(data=data, files=files, **kwargs)
 
-    def form_valid(self, form):
-        form.save(commit=True)
-        return super().form_valid(form)
+    # def form_valid(self, form):
+    #     form.save(commit=True)
+    #     return super().form_valid(form)
 
 
 post_edit = PostUpdateView.as_view()
