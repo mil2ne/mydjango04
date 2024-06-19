2f8032cdc0b9861d550f5f65393f7b8f99df7b75
celee <show3070@gmail.com>
2024-06-19 21:18:39 +0900

    #63. 장고 기본 cbv api
    formview 를 활용한 생성-수정 요청 처리

--- weblog/views.py
+++ weblog/views.py
@@ -1,46 +1,52 @@
 from django.core.files.uploadedfile import UploadedFile
 from django.shortcuts import render, redirect, get_object_or_404
 
+from vanilla import FormView
+
+# from django.views.generic import FormView
+
 from weblog.forms import PostForm
 from weblog.models import Post
 
 
-def post_new(request):
-    if request.method == "GET":
-        form = PostForm()
-    else:
-        form = PostForm(data=request.POST, files=request.FILES)
-        if form.is_valid():
-            print("form.cleand_data : ", form.cleaned_data)
+class PostCreateView(FormView):
+    form_class = PostForm
+    template_name = "weblog/post_form.html"
+    success_url = "/"
 
-            post = form.save(commit=False)
-            post.ip = request.META["REMOTE_ADDR"]
-            post.save()
-            form.save_m2m()
-
-            return redirect("/")
-        else:
-            pass
-
-    return render(request, "weblog/post_form.html", {"form": form})
+    def form_valid(self, form):
+        post = form.save(commit=False)
+        post.ip = self.request.META["REMOTE_ADDR"]
+        post.save()
+        form.save_m2m()
+        return super().form_valid(form)
 
 
-def post_edit(request, pk):
-    instance = get_object_or_404(Post, pk=pk)
+post_new = PostCreateView.as_view()
 
-    if request.method == "GET":
-        form = PostForm(instance=instance)
-    else:
-        form = PostForm(data=request.POST, files=request.FILES, instance=instance)
-        if form.is_valid():
-            print("form.cleand_data : ", form.cleaned_data)
 
-            post = form.save(commit=False)
+class PostUpdateView(FormView):
+    form_class = PostForm
+    template_name = "weblog/post_form.html"
+    success_url = "/"
 
-            post.ip = request.META["REMOTE_ADDR"]
-            post.save()
-            form.save_m2m()
+    # def get_form_kwargs(self):
+    #     post_pk = self.kwargs["pk"]
+    #     instance = get_object_or_404(Post, pk=post_pk)
+    #
+    #     form_kwargs = super().get_form_kwargs()
+    #     form_kwargs["instance"] = instance
+    #     return form_kwargs
 
-            return redirect("/")
+    def get_form(self, data=None, files=None, **kwargs):
+        post_pk = self.kwargs["pk"]
+        instance = get_object_or_404(Post, pk=post_pk)
+        kwargs["instance"] = instance
+        return super().get_form(data=data, files=files, **kwargs)
 
-    return render(request, "weblog/post_form.html", {"form": form})
+    def form_valid(self, form):
+        form.save(commit=True)
+        return super().form_valid(form)
+
+
+post_edit = PostUpdateView.as_view()
