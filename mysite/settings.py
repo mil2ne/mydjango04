a57c5a2a4b8c428141136a2e7d43f04a9a0311d7
celee <show3070@gmail.com>
2024-07-08 21:36:10 +0900

    #108. 장고 세션 인증 구현
    암호재설정 요청
    smtp 계정정보 설정 및 이메일 발송 테스트

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,8 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 import os
+import sys
 from pathlib import Path
 
+from django.core.exceptions import ImproperlyConfigured
 from django.urls import reverse_lazy
 from environ import Env
 
@@ -216,3 +218,45 @@ NAVER_MAP_POINT_WIDGET_CLIENT_ID = env.str("NAVER_MAP_POINT_WIDGET_CLIENT_ID")
 # LOGIN_REDIRECT_URL = "/accounts/profile/"
 LOGIN_REDIRECT_URL = reverse_lazy("accounts:profile")
 LOGIN_URL = reverse_lazy("accounts:login")
+
+# if DEBUG:
+#     # 이메일을 발송하지 않고, 콘솔에만 표시
+#     EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+#
+# else:
+#     # SMTP를 통해 이메일을 발송
+#     EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+#
+#     # NAVER SMTP의 경우 : 개인 이메일 서비스이므로 발송량의 제한
+#     EMAIL_HOST = "smtp.naver.com"
+#     EMAIL_PORT = 465
+#     EMAIL_USE_SSL = True
+#     EMAIL_HOST_USER = "Naver ID"  # @naver.com을 붙여도 되고 빼도 됩니다.
+#     # https://nid.naver.com/user2/help/myInfoV2?m=viewSecurity&lang=ko_KR -> 2단계 인증 관리
+#     EMAIL_HOST_PASSWORD = "8----------K"  # 네이버 암호, 2단계 인증이 설정된 경우 별도 설정된 애플리케이션 비밀번호.
+#
+#     # Google Gmail SMTP의 경우 : 개인 이메일 서비스이므로 발송량의 제한
+#     EMAIL_HOST = "smtp.gmail.com"
+#     EMAIL_PORT = 587
+#     EMAIL_USE_TLS = True
+#     EMAIL_HOST_USER = "Gmail ID"  # @gmail.com을 붙여도 되고 빼도 됩니다.
+#
+#     # 앱 비밀번호 사용을 권장 : Google 계정 (https://myaccount.google.com/security) -> 보안 -> 2단계 인증 -> 앱 비밀번호
+#     EMAIL_HOST_PASSWORD = "knjk ---- ---- igsx"
+
+EMAIL_HOST = env.str("EMAIL_HOST", default=None)
+
+if DEBUG and EMAIL_HOST is None:
+    EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+else:
+    try:
+        EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+
+        EMAIL_PORT = env.int("EMAIL_PORT")
+        EMAIL_USE_SSL = env.bool("EMAIL_USE_SSL", default=False)
+        EMAIL_USE_TLS = env.bool("EMAIL_USE_TLS", default=False)
+        EMAIL_HOST_USER = env.str("EMAIL_HOST_USER")
+        EMAIL_HOST_PASSWORD = env.str("EMAIL_HOST_PASSWORD")
+    except ImproperlyConfigured as e:
+        print("Error: ", e, file=sys.stderr)
+        EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
